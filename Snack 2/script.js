4612752a6ec58271ff92790faee6322e045e7770
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-09-09 14:21:46 +0100

    Aggiunto Snack 2

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -1,30 +1,38 @@
-// Creare un array di oggetti:
-// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-// Stampare a schermo la bici con peso minore
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 
-const bici = [
+const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min;
+
+const squadre = [
     {
-        nome: "verde",
-        pesoInKg: 7,
+        nome: "Juventus",
+        puntiFatti: getRndInteger(30, 60),
+        falliSubiti: getRndInteger(40, 80)
     },
     {
-        nome: "gialla",
-        pesoInKg: 12,
+        nome: "Cosenza",
+        puntiFatti: getRndInteger(30, 60),
+        falliSubiti: getRndInteger(40, 80)
     },
     {
-        nome: "blu",
-        pesoInKg: 9
+        nome: "Chelsea",
+        puntiFatti: getRndInteger(30, 60),
+        falliSubiti: getRndInteger(40, 80)
     }
 ];
 
-let pesopiuma = bici[0];
+console.log(squadre);
 
-for(i=0; i < bici.length; i++){
-    if( bici[i].pesoInKg < pesopiuma.pesoInKg){
-        pesopiuma = bici[i];
-    }
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+let newSquadre = [];
+
+for ( let i = 0; i < squadre.length; i++ ) {
+    const {nome, falliSubiti}  = squadre[i];
+    newSquadre.push(nome);
+    newSquadre.push(falliSubiti);
 }
 
-//destructuring bicileggera e dichiarazione
+console.log(newSquadre);
 
-console.log(`La bici più leggera è ${pesopiuma.nome} e il suo peso in kg è di ${pesopiuma.pesoInKg}`);
+
