77dfbc0319a6dc1842a97dea89a391b9ccd4e579
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-09-10 09:21:20 +0100

    Refactoring Snack 2

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -1,36 +1,48 @@
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+
 
 const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min;
 
 const squadre = [
     {
         nome: "Juventus",
-        puntiFatti: getRndInteger(30, 60),
-        falliSubiti: getRndInteger(40, 80)
+        puntiFatti: 0,
+        falliSubiti: 0    
     },
     {
         nome: "Cosenza",
-        puntiFatti: getRndInteger(30, 60),
-        falliSubiti: getRndInteger(40, 80)
+        puntiFatti: 0,
+        falliSubiti: 0    
     },
     {
         nome: "Chelsea",
-        puntiFatti: getRndInteger(30, 60),
-        falliSubiti: getRndInteger(40, 80)
+        puntiFatti: 0,
+        falliSubiti: 0    
     }
 ];
 
 console.log(squadre);
 
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-let newSquadre = [];
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 
 for ( let i = 0; i < squadre.length; i++ ) {
     const {nome, falliSubiti}  = squadre[i];
-    newSquadre.push(nome);
-    newSquadre.push(falliSubiti);
+    squadre[i].puntiFatti = getRndInteger(30, 60);
+    squadre[i].falliSubiti = getRndInteger(15, 30);
+}
+
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const newSquadre = [];
+
+for ( let i = 0; i < squadre.length; i++ ) {
+    const {nome, falliSubiti}  = squadre[i];
+    const squadra = {
+        nome,
+        falliSubiti
+    };
+    newSquadre.push(squadra);
 }
 
 console.log(newSquadre);
